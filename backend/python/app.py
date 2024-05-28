a60924092baf0871fbf55d5016c0e6b4f3042f08
Arthur <90999445+arthvm@users.noreply.github.com>
2024-05-28 18:06:43 -0300

    Change icon color according to severity

--- backend/python/app.py
+++ backend/python/app.py
@@ -1,6 +1,8 @@
 import os
 import folium
+from folium.plugins import MarkerCluster
 from flask import Flask, json, render_template
+
 
 app = Flask(__name__)
 
@@ -15,20 +17,33 @@ def load_data():
 @app.route("/map")
 def create_map():
     map = folium.Map(location=[-23.5489, -46.6388], tiles='CartoDB.Voyager')
+    marker_cluster = MarkerCluster().add_to(map)
 
     reports = load_data();
     for report in reports:
         popup_html = f"""
-        <span>Registered by: {report["user"] if not None else "Anonymous"}</span>
+        <span>Registered by: {report["user"] if not "None" else "Anonymous"}</span>
         <ul>
             <li>Descrição: {report["description"]}</li>
             <li>Gravidade: {report["severity"]}</li>
         </ul>
         """
+
+        match report["severity"]:
+            case "high":
+                severity_color = "red"
+            case "medium":
+                severity_color = "orange"
+            case _:
+                severity_color = "green"
+
         folium.Marker(
             location=[report["lat"], report["lng"]],
-            popup=popup_html
-        ).add_to(map)
+            popup=popup_html,
+            icon=folium.Icon(
+                color=severity_color
+            )
+        ).add_to(marker_cluster)
 
     map.save("templates/map.html")
     return render_template("map.html")
