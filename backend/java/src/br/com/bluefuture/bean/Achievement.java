69cfa6746a008bed9a23f028a3d8e6637c179030
Arthur <90999445+arthvm@users.noreply.github.com>
2024-05-31 20:40:21 -0300

    Add basic Alert and Achievement class

--- backend/java/src/br/com/bluefuture/bean/Achievement.java
+++ backend/java/src/br/com/bluefuture/bean/Achievement.java
@@ -1,4 +1,39 @@
 package br.com.bluefuture.bean;
 
-public class Achievement {
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public abstract class Achievement {
+    private String name;
+    private String description;
+
+    public Achievement() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        if (name == null) throw new IllegalArgumentException("Name of achievement can't be empty");
+
+        String regex = "^[A-Za-zÀ-ÖØ-öø-ÿ]{2,}$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(name);
+
+        if (!matcher.matches()) throw new IllegalArgumentException("Name of the achievement must contain at least 2 " +
+                "letters and can include accents.");
+
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public abstract void unlock();
 }
--- backend/java/src/br/com/bluefuture/bean/Alert.java
+++ backend/java/src/br/com/bluefuture/bean/Alert.java
@@ -1,4 +1,65 @@
 package br.com.bluefuture.bean;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Alert {
+    private String location;
+    private String severity;
+
+    public Alert() {
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        try {
+            if (location == null) throw new IllegalArgumentException("Invalid Argument --> Expected a non-null String");
+
+            String cep;
+            String regex = "\\d{5}-?\\d{3}";
+            Pattern pattern = Pattern.compile(regex);
+            Matcher matcher = pattern.matcher(location);
+
+            if(!(matcher.find())) throw new IllegalArgumentException("Invalid Argument --> CEP was not" +
+                    " found in the String. Make sure it has 8 digits (Dash is optional)");
+
+            cep = matcher.group(0);
+
+            HttpClient client = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(String.format("https://viacep.com.br/ws/%s/json", cep))).build();
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+            if (response.statusCode() != 200) throw new IOException("Failed to connect with the API --> " +
+                    response.statusCode());
+
+            if(response.body().contains("\"erro\": true")) throw new IllegalArgumentException("Invalid Argument --> " +
+                    "Given CEP was not valid");
+
+            this.location = location;
+
+        }catch (IllegalArgumentException e){
+            System.err.println(e.getMessage());
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public String getSeverity() {
+        return severity;
+    }
+
+    public void setSeverity(String severity) {
+        if(severity == null) throw new IllegalArgumentException("Severity can't be null");
+
+        this.severity = severity;
+    }
 }
