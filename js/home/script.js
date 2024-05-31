a1f581e3d0315a57deae9a1005b95299c2248ea3
guimaggiorini <guihm929@gmail.com>
2024-05-31 09:46:06 -0300

    Add ChatBot dropdown using iFrame

--- js/home/script.js
+++ js/home/script.js
@@ -8,13 +8,20 @@ document.addEventListener('DOMContentLoaded', function () {
     const dropdownContent = document.getElementById('dropdown-content')
     const rotateIcon = document.getElementById('rotate-icon')
     const severityOptions = document.querySelectorAll('.dropdown-option')
+    const chatbotButton = document.getElementById('chatbot-button')
+    const chatbotContent = document.getElementById('chatbot-content')
+    const sidebarReport = document.querySelector('.sidebar-report')
+    const chatbotDropdownIcon = document.getElementById('chatbot-dropdown-icon')
 
     /* Remove the "ghost space" when the page is loaded */
     sidebar.classList.add('display-none')
 
+    /* Ensuring the close sidebar icon doesn't show when the page is loaded */
+    closeIcon.style.display = 'none'
+
     /* When the hamburger icon is clicked, the sidebar appears and the map is reduced to 76vw */
     hamburgerIcon.addEventListener('click', function () {
-
+        
         sidebar.classList.remove('display-none')
 
         /* This timeout is used to prevent all those functions loading with the "display-none" class removal */
@@ -46,7 +53,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
     /* When the severity button is clicked, the dropdown content appears */
     severityDropdown.addEventListener('click', function () {
-
+        
         dropdownContent.classList.toggle('show-dropdown-content')
         severityDropdown.classList.toggle('dropdown-active')
         rotateIcon.classList.toggle('rotate-icon')
@@ -66,12 +73,43 @@ document.addEventListener('DOMContentLoaded', function () {
                 <img src="./assets/severity-icon.png" alt="" class="severity-icon">
                 ${severity} Severity
             `
-
             dropdownContent.classList.remove('show-dropdown-content')
             severityDropdown.classList.remove('dropdown-active')
             rotateIcon.classList.remove('rotate-icon')
 
         })
+        
+    })
+
+    /* When the chatbot dropdown is clicked, toggle the chatbot content and hide the sidebar report */
+    chatbotButton.addEventListener('click', function () {
+
+        if (chatbotContent.classList.contains('show-chatbot-content')) {
+
+            chatbotContent.classList.remove('show-chatbot-content')
+
+            /* This timeout is used to ensure the report container appears after the chatbot container closes */
+            setTimeout(() => {
+                chatbotContent.style.display = 'none'
+                sidebarReport.classList.remove('hidden-sidebar-report')
+            }, 400)
+
+        } else {
+
+            chatbotContent.style.display = 'flex'
+
+            /* This timeout is used to ensure the animation loads after the chatbot container appears */
+            setTimeout(() => {
+                chatbotContent.classList.add('show-chatbot-content')
+            }, 1)
+
+            sidebarReport.classList.add('hidden-sidebar-report')
+
+        }
+
+        chatbotButton.classList.toggle('chatbot-active')
+        chatbotDropdownIcon.classList.toggle('rotate-icon')
 
     })
+
 })
